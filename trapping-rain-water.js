1da4872416c2432a48f05a26595197bdadd00064
ishowshao <ishowshao@me.com>
2022-04-02 15:20:09 +0800

    Update trapping-rain-water.js

--- trapping-rain-water.js
+++ trapping-rain-water.js
@@ -1,49 +1,39 @@
-// 这个方法会超时...
 /**
  * @param {number[]} height
  * @return {number}
  */
-var trap = function (height) {
-    let total = 0;
-    let levelArr = [];
-    let levelCount = 0;
-    let has = -1;
-    do {
-        for (let i = 0; i < height.length; i++) {
-            let h = height[i];
-            if (h === 0) {
-                levelArr.push(0);
-            } else {
-                levelArr.push(1);
-                height[i] -= 1;
-            }
-        }
-        console.log(levelArr);
-        [levelCount, has] = count(levelArr);
-        total += levelCount;
-        // console.log(height);
-        levelArr = [];
-    } while (has > 0)
-    return total;
-};
-
-/**
- * @param {Array} arr 
- */
-const count = (arr) => {
-    let start = arr.indexOf(1);
-    let end = arr.lastIndexOf(1);
-    let zeroCount = 0;
-    let oneCount = 0;
-    for (let i = start; i < end; i++) {
-        if (arr[i] === 0) {
-            zeroCount++;
-        } else {
-            oneCount++;
+ var trap = function (height) {
+    let noWater = 0;
+    let maxHeight = 0;
+    let maxHeightIndex = -1;
+    for (let i = 0, length = height.length; i < length; i++) {
+        let h = height[i];
+        noWater += h;
+        if (h > maxHeight) {
+            maxHeight = h;
+            maxHeightIndex = i;
         }
     }
 
-    return [zeroCount, oneCount];
-};
+    let left = 0;
+    let currentHeight = 0;
+    for (let i = 0; i < maxHeightIndex; i++) {
+        let h = height[i];
+        if (h > currentHeight) {
+            currentHeight = h;
+        }
+        left += currentHeight;
+    }
 
-console.log(trap([4, 2, 3]));
+    let right = 0;
+    currentHeight = 0;
+    for (let i = height.length - 1; i >= maxHeightIndex; i--) {
+        let h = height[i];
+        if (h > currentHeight) {
+            currentHeight = h;
+        }
+        right += currentHeight;
+    }
+
+    return left + right - noWater;
+};
