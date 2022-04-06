7714ee0dd2d4e0460076f6343218c9a27bcd4f92
ishowshao <ishowshao@me.com>
2022-04-06 11:58:50 +0800

    Update jump-game.js

--- jump-game.js
+++ jump-game.js
@@ -4,5 +4,25 @@
  * @return {boolean}
  */
 var canJump = function (nums) {
-
+    let j = 0;
+    for (let i = nums.length - 2; i > -1; i--) {
+        let num = nums[i];
+        if (num !== 0) {
+            continue;
+        }
+        let can = false;
+        for (j = i - 1; j > -1; j--) {
+            if (nums[j] > i - j) {
+                can = true;
+                i = j;
+                break;
+            }
+        }
+        if (!can) {
+            return can;
+        }
+    }
+    return true;
 };
+
+console.log(canJump([3, 2, 1, 0, 4]));
