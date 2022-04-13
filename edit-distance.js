3b89c2353058cbd917087ae680e4323ac152f573
ishowshao <ishowshao@me.com>
2022-04-13 17:57:19 +0800

    Update edit-distance.js

--- edit-distance.js
+++ edit-distance.js
@@ -4,85 +4,37 @@
  * @return {number}
  */
 var minDistance = function (word1, word2) {
-    word1 = word1.split('');
-    word2 = word2.split('');
+    // 不看答案能猜出来是动态规划，但是没想明白怎么规划
+    // 看了答案，感觉最重要的是理解从空字符串到n长度字符怎么规划
+    // 然后从1长度字符到n怎么规划
+    // 最后从m到n怎么规划
+    // 先在纸上手动解一个例子再写代码会清晰很多
+    const dp = Array(word1.length + 1).fill(0).map(() => Array(word2.length + 1).fill(0));
 
-    let min = Math.max(word1.length, word2.length);
+    let m = dp.length;
+    let n = dp[0].length;
+    for (let i = 0; i < m; i++) {
+        dp[i][0] = i;
+    }
+    for (let j = 0; j < n; j++) {
+        dp[0][j] = j;
+    }
 
-    const helper = (from) => {
-        // 如果 word1.length > word2.length ，插入字符肯定直接放弃
-        let d;
-        if (from.length < word2.length) {
-            d = Math.min(
-                distance('rhorse', word2),
-                distance('orse', word2),
-                distance('rorse', word2)
-            ) + 1;
-        } else {
-            d = Math.min(
-                distance('orse', word2),
-                distance('rorse', word2)
-            ) + 1;
-        }
-    };
+    // console.log(dp);
 
-    const distance = (a, b) => {
-        let d = Math.max(a.length, b.length);
-        let s = Math.min(a.length, b.length);
-        for (let i = 0; i < s; i++) {
-            if (a[i] === b[i]) {
-                d--;
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (word1[i - 1] !== word2[j - 1]) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+            } else {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] - 1) + 1;
             }
         }
-        return d;
-    };
-
-
-    const insert = (word) => {
-        let i = 0;
-        while (word[i] === word2[i]) {
-            i++;
-        }
-        word.splice(i, 0, word2[i]);
-        return word;
-    };
-
-    /**
-     * @param {Array} word 
-     * @returns 
-     */
-    const del = (word) => {
-        word.shift();
-        return word;
-    };
-
-    const replace = (word) => {
-        let i = 0;
-        while (word[i] === word2[i]) {
-            i++;
-        }
-        word[i] = word2[i];
-        return word;
-    };
-};
-
-const distance = (a, b) => {
-    let d = Math.max(a.length, b.length);
-    let s = Math.min(a.length, b.length);
-    for (let i = 0; i < s; i++) {
-        if (a[i] === b[i]) {
-            d--;
-        }
     }
-    return d;
+
+    // console.log(dp);
+    return dp[m - 1][n - 1];
 };
 
-const str = 'abcde';
 
-for (let i = 0; i < str.length; i++) {
-    let a = str.substring(0, i) + str.substring(i + 1);
-    console.log(a, distance(a, 'acde'));
-}
-
-
-console.log(distance('abcde', 'abccdde'));
+console.log(minDistance('abcde', 'bcdea'));
