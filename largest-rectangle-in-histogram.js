f120f7e2bb2acd72b8984a60a6ce229b03951b2c
ishowshao <ishowshao@me.com>
2022-04-14 12:09:07 +0800

    用单调栈把这个题做了

--- largest-rectangle-in-histogram.js
+++ largest-rectangle-in-histogram.js
@@ -1,19 +1,37 @@
+// 用单调栈把这个题做了
 /**
  * @param {number[]} heights
  * @return {number}
  */
 var largestRectangleArea = function (heights) {
-    const minHeights = Array(heights.length).fill(0);
-    minHeights[heights.length - 1] = heights[heights.length - 1];
-    for (let i = heights.length - 2; i > -1; i--) {
+    const leftEdge = Array(heights.length).fill(0);
+    let stack = [{ value: -1, index: -1 }];
+    for (let i = 0; i < heights.length; i++) {
         let height = heights[i];
-        if (height < minHeights[i + 1]) {
-            minHeights[i] = height;
-        } else {
-            minHeights[i] = minHeights[i + 1];
+        while (stack[stack.length - 1].value >= height) {
+            stack.pop();
         }
+        leftEdge[i] = stack[stack.length - 1].index;
+        stack.push({ value: height, index: i });
     }
-    console.log(minHeights);
+    // console.log(leftEdge);
+    const rightEdge = Array(heights.length).fill(0);
+    stack = [{ value: -1, index: heights.length }];
+    for (let i = heights.length - 1; i > -1; i--) {
+        let height = heights[i];
+        while (stack[stack.length - 1].value >= height) {
+            stack.pop();
+        }
+        rightEdge[i] = stack[stack.length - 1].index;
+        stack.push({ value: height, index: i });
+    }
+    // console.log(rightEdge);
+    let max = 0;
+    for (let i = 0; i < heights.length; i++) {
+        let height = heights[i];
+        max = Math.max(max, (rightEdge[i] - leftEdge[i] - 1) * height);
+    }
+    return max;
 };
 
 console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]));
