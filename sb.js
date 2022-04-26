0063daaa758168fd77f6b3bbdbce5f81fe312659
ishowshao <ishowshao@me.com>
2022-04-26 12:20:46 +0800

    1

--- sb.js
+++ sb.js
@@ -1,6 +1,6 @@
 let total = 0;
 let top = 27719;
-let rt = 0.96442;
+let rt = 0.9553;
 for (let i = 0; i < 156; i++) {
     console.log(i, Math.pow(rt, i) * top);
     total += Math.pow(rt, i) * top;
