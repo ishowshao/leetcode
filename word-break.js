f18e7a47673372e3c51047a9a277ee3d05441f24
ishowshao <ishowshao@me.com>
2022-04-26 16:41:27 +0800

    Update word-break.js

--- word-break.js
+++ word-break.js
@@ -4,30 +4,44 @@
  * @return {boolean}
  */
 var wordBreak = function (s, wordDict) {
-    let result = false;
-    let slen = s.length;
-    let r = 0;
-    const helper = (position) => {
-        r++;
-        if (result) {
-            return;
-        }
-        if (position === slen) {
-            result = true;
-            return;
-        }
-        for (let i = 0; i < wordDict.length; i++) {
-            if (match(s, position, wordDict[i])) {
-                let length = wordDict[i].length
-                position += length;
-                helper(position);
-                position -= length;
+    const n = s.length;
+    const wordDictSet = new Set(wordDict);
+    const dp = new Array(n + 1).fill(false);
+    dp[0] = true;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (dp[j] && wordDictSet.has(s.substr(j, i - j))) {
+                dp[i] = true;
+                break;
             }
         }
-    };
-    helper(0);
-    console.log(r);
-    return result;
+    }
+    return dp[n];
+
+    // let result = false;
+    // let slen = s.length;
+    // let r = 0;
+    // const helper = (position) => {
+    //     r++;
+    //     if (result) {
+    //         return;
+    //     }
+    //     if (position === slen) {
+    //         result = true;
+    //         return;
+    //     }
+    //     for (let i = 0; i < wordDict.length; i++) {
+    //         if (match(s, position, wordDict[i])) {
+    //             let length = wordDict[i].length
+    //             position += length;
+    //             helper(position);
+    //             position -= length;
+    //         }
+    //     }
+    // };
+    // helper(0);
+    // console.log(r);
+    // return result;
 };
 
 const match = (s, position, word) => {
@@ -44,7 +58,7 @@ const match = (s, position, word) => {
 // 这个方法会超时，最主要的问题是 最后一个用例，其实wordDict里面 a aa aaa ...都可以简化为a，就不会超时
 
 console.log(wordBreak('leetcode', ["leet", "code"]));
-console.log(wordBreak('applepenapple', ["apple", "pen"]));
-console.log(wordBreak('catsandog', ["cats", "dog", "sand", "and", "cat"]));
-console.log(wordBreak('aaaaaaaaaaaaab', ['aa', 'a']));
+// console.log(wordBreak('applepenapple', ["apple", "pen"]));
+// console.log(wordBreak('catsandog', ["cats", "dog", "sand", "and", "cat"]));
+// console.log(wordBreak('aaaaaaaaaaaaab', ['aa', 'a']));
 // console.log(wordBreak('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab', ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]));
