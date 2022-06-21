1f5a8bd743ab181700109ea55646c60db616a9f2
ishowshao <234047485@qq.com>
2022-06-21 16:24:02 +0800

    Update find-the-duplicate-number.js

--- find-the-duplicate-number.js
+++ find-the-duplicate-number.js
@@ -3,5 +3,28 @@
  * @return {number}
  */
 var findDuplicate = function (nums) {
-    
+    let slow = 0;
+    let fast = 0;
+    // step 1
+    while (true) {
+        slow = nums[slow];
+        fast = nums[fast];
+        fast = nums[fast];
+        if (slow === fast) {
+            break;
+        }
+    }
+    console.log(slow, fast);
+    slow = 0;
+    while (true) {
+        slow = nums[slow];
+        fast = nums[fast];
+        if (slow === fast) {
+            break;
+        }
+    }
+    return slow;
 };
+
+console.log(findDuplicate([1, 3, 4, 2, 2]));
+// console.log(findDuplicate([3, 1, 3, 4, 2]));
