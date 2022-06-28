9cbddcafeb6e8cfec1821f2ded03b899dc95eadd
ishowshao <234047485@qq.com>
2022-06-28 17:06:32 +0800

    Update coin-change.js

--- coin-change.js
+++ coin-change.js
@@ -14,9 +14,15 @@ var coinChange = function (coins, amount) {
 
     const target = [];
 
+    const map = {};
+
     const helper = (amount) => {
         if (amount === 0) {
-            result.push([...target]);
+            result.push(target.length);
+            return;
+        }
+        if (map[amount] !== undefined) {
+            result.push(target.length + map[amount]);
             return;
         }
         for (let i = 0; i < coins.length; i++) {
@@ -29,10 +35,22 @@ var coinChange = function (coins, amount) {
         }
     };
 
-    helper(amount);
+    // helper(amount);
 
-    console.log(result);
+    for (let i = 1; i <= amount; i++) {
+        helper(i);
+        // console.log(result);
+        map[i] = Math.min(...result);
+        result = [];
+        // break;
+    }
+
+    // console.log(map);
+    return Number.isFinite(map[amount]) ? map[amount] : -1;
+
+    // console.log(result);
 };
 
-console.log(coinChange([1, 2, 5], 11));
-// console.log(coinChange([186, 419, 83, 408], 6249));
+// console.log(coinChange([1, 2, 5], 11));
+console.log(coinChange([186, 419, 83, 408], 6249));
+console.log(coinChange([2], 3));
