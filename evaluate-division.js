fc9999d9575c558fb8a536e1ec3dde7acfee9fd9
ishowshao <234047485@qq.com>
2022-07-01 17:59:20 +0800

    Update evaluate-division.js

--- evaluate-division.js
+++ evaluate-division.js
@@ -5,6 +5,7 @@
  * @return {number[]}
  */
 var calcEquation = function (equations, values, queries) {
+    // 需要构建一个这样的结构
     // const map = {
     //     a: {
     //         root: 'c',
@@ -23,97 +24,76 @@ var calcEquation = function (equations, values, queries) {
     //         weight: 1,
     //     },
     // };
+    // a - b - c
+    //     2   3
+    // 找到root，更新root和weight
+    const find = (node) => {
+        let root = node.root;
+        let weight = node.weight;
+        while (root !== map[root].root) {
+            weight *= map[root].weight;
+            root = map[root].root;
+        }
+        node.root = root;
+        node.weight = weight;
+        return node;
+    };
 
-    const equationsSize = equations.length;
-
-    const unionFind = new UnionFind(equationsSize * 2);
-    const hashMap = {};
-
-    let id = 0;
-    for (let i = 0; i < equationsSize; i++) {
+    const map = {};
+    // 初始化
+    for (let i = 0; i < equations.length; i++) {
         const equation = equations[i];
-        const [var1, var2] = equation;
-        if (hashMap[var1] === undefined) {
-            hashMap[var1] = id;
-            id++;
+        const [s1, s2] = equation;
+        map[s1] = { root: s2, weight: values[i] };
+        if (map[s2] === undefined) {
+            map[s2] = { root: s2, weight: 1 };
         }
-        if (hashMap[var2] === undefined) {
-            hashMap[var2] = id;
-            id++;
-        }
-        unionFind.union(hashMap[var1], hashMap[var2], values[i]);
     }
-    // return result;
+
+    for (let i = 0; i < equations.length; i++) {
+        const equation = equations[i];
+        const [s1, s2] = equation;
+        const root1 = find(map[s1]);
+        // console.log('s1', s1, root1);
+
+        const root2 = find(map[s2]);
+        // console.log('s2', s2, root2);
+
+        // 这个地方如何理解：
+        // 按照循环找root找出来的root，竟然跟声明告诉的不一致
+        // 那么找出来的root1一定能连到声明告诉的root2
+        if (root1.root !== root2.root) {
+            map[root1.root].root = root2.root;
+            map[root1.root].weight = (root2.weight * values[i]) / root1.weight;
+        }
+    }
+    console.log(map);
+
     const result = [];
     for (let i = 0; i < queries.length; i++) {
-        let var1 = queries[i][0];
-        let var2 = queries[i][1];
-
-        let id1 = hashMap[var1];
-        let id2 = hashMap[var2];
-
-        if (id1 == null || id2 == null) {
-            result[i] = -1;
+        const [s1, s2] = queries[i];
+        if (map[s1] === undefined || map[s2] === undefined) {
+            result.push(-1);
         } else {
-            result[i] = unionFind.isConnected(id1, id2);
+            const r1 = find(map[s1]);
+            const r2 = find(map[s2]);
+            if (r1.root === r2.root) {
+                result.push(r1.weight / r2.weight);
+            } else {
+                result.push(-1);
+            }
         }
     }
     return result;
 };
 
-class UnionFind {
-    constructor(n) {
-        this.parent = Array(n);
-        this.weight = Array(n);
-        for (let i = 0; i < n; i++) {
-            this.parent[i] = i;
-            this.weight[i] = 1;
-        }
-    }
-
-    union(x, y, value) {
-        let rootX = this.find(x);
-        let rootY = this.find(y);
-        if (rootX == rootY) {
-            return;
-        }
-
-        this.parent[rootX] = rootY;
-        // 关系式的推导请见「参考代码」下方的示意图
-        this.weight[rootX] = (this.weight[y] * value) / this.weight[x];
-    }
-
-    find(x) {
-        if (x != this.parent[x]) {
-            let origin = this.parent[x];
-            this.parent[x] = this.find(this.parent[x]);
-            this.weight[x] *= this.weight[origin];
-        }
-        return this.parent[x];
-    }
-
-    isConnected(x, y) {
-        let rootX = this.find(x);
-        let rootY = this.find(y);
-        if (rootX == rootY) {
-            return this.weight[x] / this.weight[y];
-        } else {
-            return -1;
-        }
-    }
-}
-
 console.log(
     calcEquation(
         [
-            ['a', 'd'],
             ['a', 'b'],
-            ['d', 'c'],
-            ['e', 'f']
+            ['c', 'd'],
         ],
-        [3.0, 4.0, 6.0, 2.0],
-        [
-            ['a', 'c'],
-        ]
+        [3.4, 1.4, 2.3],
+        [['a', 'c']]
     )
 );
