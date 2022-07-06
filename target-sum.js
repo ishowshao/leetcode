a42e294944db7a7c1b1e8c2a270bc23c66b8b14a
ishowshao <234047485@qq.com>
2022-07-06 17:13:19 +0800

    Create target-sum.js

---
+++ target-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var findTargetSumWays = function (nums, target) {
+    const a = [0];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0, len = a.length; j < len; j++) {
+            a.push(a[j] + nums[i]);
+            a[j] -= nums[i];
+        }
+        console.log(a);
+    }
+    console.log(a);
+    return a.filter((e) => e === target).length;
+};
+
+console.log(findTargetSumWays([1, 1, 1, 1, 1]));
