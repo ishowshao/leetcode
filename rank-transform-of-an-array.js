9b4de124dd8b7a3ae0e566c25a58717dc54b9a7c
ishowshao <234047485@qq.com>
2022-07-28 17:14:11 +0800

    Update rank-transform-of-an-array.js

--- rank-transform-of-an-array.js
+++ rank-transform-of-an-array.js
@@ -6,5 +6,23 @@ var arrayRankTransform = function (arr) {
     const set = new Set(arr);
     const arrNew = Array.from(set);
     arrNew.sort((a, b) => a - b);
-    return arr.map(el => arrNew.indexOf(el) + 1);
+    return arr.map((el) => search(arrNew, el) + 1);
 };
+
+const search = (array, n) => {
+    let left = 0;
+    let right = array.length - 1;
+    while (left <= right) {
+        let mid = (left + right) >> 1;
+        if (array[mid] === n) {
+            return mid;
+        }
+        if (array[mid] < n) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+};
+
+console.log(arrayRankTransform([40, 10, 20, 30]));
