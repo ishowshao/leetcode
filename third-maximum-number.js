9c798eb8665cf4f886b1a5d58bfbd1c9361e6fd8
ishowshao <234047485@qq.com>
2022-08-02 18:56:59 +0800

    Update third-maximum-number.js

--- third-maximum-number.js
+++ third-maximum-number.js
@@ -3,7 +3,24 @@
  * @return {number}
  */
 var thirdMax = function (nums) {
-    nums = Array.from(new Set(nums));
-    nums = nums.sort((a, b) => b - a);
-    return nums[2] === undefined ? nums[0] : nums[2];
+    const set = [Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY];
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        for (let j = 0; j < set.length; j++) {
+            if (num === set[j]) {
+                break;
+            }
+            if (num > set[j]) {
+                set.splice(j, 0, num);
+                set.pop();
+                break;
+            }
+        }
+    }
+    console.log(set);
+    return isFinite(set[2]) ? set[2] : set[0];
 };
+
+console.log(thirdMax([3, 2, 1]));
+console.log(thirdMax([2, 2, 3, 1]));
+console.log(thirdMax([1, 2, 2]));
