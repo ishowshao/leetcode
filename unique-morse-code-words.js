053f2a21eaed978ef94d9498510fe7fd31667eea
ishowshao <234047485@qq.com>
2022-08-04 17:41:26 +0800

    Create unique-morse-code-words.js

---
+++ unique-morse-code-words.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+const MORSE = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..'];
+var uniqueMorseRepresentations = function (words) {
+    const seen = new Set();
+    for (const word of words) {
+        let code = '';
+        for (const ch of word) {
+            code += MORSE[ch.charCodeAt() - 'a'.charCodeAt()];
+        }
+        seen.add(code);
+    }
+    return seen.size;
+};
