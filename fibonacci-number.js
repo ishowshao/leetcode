2359adaf933fd04964d715fe440240ecf093832e
ishowshao <234047485@qq.com>
2022-08-04 13:18:26 +0800

    Create fibonacci-number.js

---
+++ fibonacci-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+    const nums = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        nums[i] = nums[i - 1] + nums[i - 2];
+    }
+    return nums[n];
+};
