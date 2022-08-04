2cf0c24026780a15c65ac79a7acea144eaa8b055
ishowshao <234047485@qq.com>
2022-08-04 18:12:16 +0800

    Create most-common-word.js

---
+++ most-common-word.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function (paragraph, banned) {
+    const bannedSet = new Set();
+    for (const word of banned) {
+        bannedSet.add(word);
+    }
+    let maxFrequency = 0;
+    const frequencies = new Map();
+    let sb = '';
+    const length = paragraph.length;
+    for (let i = 0; i <= length; i++) {
+        if (i < length && isLetter(paragraph[i])) {
+            sb = sb + paragraph[i].toLowerCase();
+        } else if (sb.length > 0) {
+            if (!bannedSet.has(sb)) {
+                const frequency = (frequencies.get(sb) || 0) + 1;
+                frequencies.set(sb, frequency);
+                maxFrequency = Math.max(maxFrequency, frequency);
+            }
+            sb = '';
+        }
+    }
+    let mostCommon = '';
+    for (const [word, frequency] of frequencies.entries()) {
+        if (frequency === maxFrequency) {
+            mostCommon = word;
+            break;
+        }
+    }
+    return mostCommon;
+};
+
+const isLetter = (ch) => {
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+};
