3ec3aa35c0548775ede125a678e735da24b8b6a0
ishowshao <234047485@qq.com>
2022-08-24 14:16:55 +0800

    Create first-bad-version.js

---
+++ first-bad-version.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        const search = (i, j) => {
+            const mid = Math.floor((i + j) / 2);
+        };
+    };
+};
