e290ab93224c3a043aa8147a5d95218ee96c9e40
ishowshao <234047485@qq.com>
2022-09-02 16:48:56 +0800

    Update date.js

--- date.js
+++ date.js
@@ -1,7 +1,7 @@
 let times = [
-    { str: '2022-09-02 11:11:11', timestamp: 0 },
-    { str: '2022-09-03 11:11:11', timestamp: 0 },
-    { str: '2022-09-04 00:00:00', timestamp: 0 },
+    { str: '2022-08-02 11:11:11', timestamp: 0 },
+    { str: '2022-08-03 11:11:11', timestamp: 0 },
+    { str: '2022-08-04 00:00:00', timestamp: 0 },
     { str: '2022-09-05 11:11:11', timestamp: 0 },
     { str: '2022-09-10 11:11:11', timestamp: 0 },
     { str: '2022-09-11 11:11:11', timestamp: 0 },
@@ -23,10 +23,25 @@ let g = [];
 let gDiv = 0;
 // 周一作为第一天用259200000
 // 周一作为第一天用172800000
-const weekOffset = 172800000;
+// const weekOffset = 172800000;
+// times.forEach(r => {
+//     const t = r.timestamp + offset - weekOffset;
+//     const div = Math.floor(t / 604800000);
+//     if (gDiv === div) {
+//         g.push(r);
+//     } else {
+//         group.push(g);
+//         g = [r];
+//         gDiv = div;
+//     }
+// });
+// group.push(g);
+// group.shift();
+
+// console.log(group);
+
 times.forEach(r => {
-    const t = r.timestamp + offset - weekOffset;
-    const div = Math.floor(t / 604800000);
+    const div = r.year * 10 + r.month;
     if (gDiv === div) {
         g.push(r);
     } else {
@@ -37,5 +52,4 @@ times.forEach(r => {
 });
 group.push(g);
 group.shift();
-
 console.log(group);
