9c3fb2795fcf6e317f7232877b26d6ec8c646fdf
ishowshao <ishowshao@me.com>
2022-10-01 22:02:55 +0800

    Update 4sum-ii.js

--- 4sum-ii.js
+++ 4sum-ii.js
@@ -5,24 +5,20 @@
  * @param {number[]} nums4
  * @return {number}
  */
-var fourSumCount = function (nums1, nums2, nums3, nums4) {
-    const map4 = {};
+ var fourSumCount = function (nums1, nums2, nums3, nums4) {
+    const map4 = new Map();
     for (let i = 0; i < nums3.length; i++) {
         for (let j = 0; j < nums4.length; j++) {
             let sum = nums3[i] + nums4[j];
-            if (map4[sum] === undefined) {
-                map4[sum] = 1;
-            } else {
-                map4[sum]++;
-            }
+            map4.set(sum, map4.has(sum) ? map4.get(sum) + 1 : 1);
         }
     }
     let count = 0;
     for (let i = 0; i < nums1.length; i++) {
         for (let j = 0; j < nums2.length; j++) {
             let sum = nums1[i] + nums2[j];
-            if (map4[0 - sum] !== undefined) {
-                count += map4[0 - sum];
+            if (map4.has(0 - sum)) {
+                count += map4.get(0 - sum);
             }
         }
     }
