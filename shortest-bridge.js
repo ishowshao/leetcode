c551f22beaa9121324d4343dd0ffe479b6755c9e
ishowshao <234047485@qq.com>
2022-12-01 15:42:45 +0800

    Create shortest-bridge.js

---
+++ shortest-bridge.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var shortestBridge = function (grid) {
+    return 1;
+};
+
+console.log(
+    shortestBridge([
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+    ])
+);
