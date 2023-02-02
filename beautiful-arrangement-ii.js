b785aa398697f75f45b168adc9e297ca9ded58c5
ishowshao <234047485@qq.com>
2023-02-02 16:36:53 +0800

    Update beautiful-arrangement-ii.js

--- beautiful-arrangement-ii.js
+++ beautiful-arrangement-ii.js
@@ -4,38 +4,17 @@
  * @return {number[]}
  */
 var constructArray = function (n, k) {
-    if (k === 1) {
-        const ans = Array(n).fill(0);
-        ans.forEach((v, i) => (ans[i] = i + 1));
-        return ans;
-    } else {
-        const ans = [];
-        const array = Array(n).fill(0);
-        array.forEach((v, i) => (array[i] = i + 1));
-        array[0] = 0;
-        array[k] = 0;
-
-        ans[0] = 1;
-        ans[1] = k + 1;
-        k--;
-        while (k > 0) {
-            console.log(k);
-            let num = ans[ans.length - 1];
-            let test = num - k;
-            if (test > 0 && !ans.includes(test)) {
-                ans.push(test);
-            } else {
-                ans.push(num + k);
-            }
-            array[ans[ans.length - 1] - 1] = 0;
-            k--;
+    const ans = [1];
+    for (let i = 0; i < k; i++) {
+        if (i % 2 === 0) {
+            ans.push(k + 1 - Math.floor(i / 2));
+        } else {
+            ans.push(2 + Math.floor(i / 2));
         }
-        array.forEach((v) => {
-            if (v !== 0) {
-                ans.push(v);
-            }
-        });
-        return ans;
     }
+    while (++k < n) {
+        ans.push(k + 1);
+    }
+    return ans;
 };
-console.log(constructArray(10, 9));
+console.log(constructArray(5, 1));
